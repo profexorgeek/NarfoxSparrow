e15b39cf0a8c338677e9a2f1b1aa69b89f4c00bb
Justin Johnson <justin.d.johnson@gmail.com>
2021-09-16 16:55:26 -0600

    Abstracted gathering user input into a service

---
+++ NarfoxSparrow/Services/InputService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NarfoxSparrow.Services
+{
+    public class InputService
+    {
+        static InputService instance;
+
+        public static InputService Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new InputService();
+                }
+                return instance;
+            }
+        }
+
+        private InputService() { }
+
+        public string GetUserInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            var input = Console.ReadLine();
+            return input;
+        }
+    }
+}
